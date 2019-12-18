f83b2ce23a2e17808c859fdb42055b7640e0c5b4
BradleyJrichardson <brdl.richardson@gmail.com>
2019-12-18 16:19:23 +1100

    .

--- src/components/sections/Contact/index.js
+++ src/components/sections/Contact/index.js
@@ -3,13 +3,11 @@ import { Container } from 'components/common'
 import contact from 'assets/illustrations/contact.svg'
 import { Wrapper, Details, Thumbnail } from './styles'
 import ContactForm from './ContactForm'
-import ContactTest from './ContactTest'
 
 export const Contact = () => (
   <Wrapper as={Container} id="contact">
     <Details>
       <ContactForm />
-      <ContactTest />
     </Details>
     <Thumbnail>
       <img src={contact} alt="I’m John and I’m a Backend & Devops engineer!" />
