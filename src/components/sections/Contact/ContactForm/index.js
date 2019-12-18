3d632c34aef814f48e2f2414d463e33b3b88d44d
BradleyJrichardson <brdl.richardson@gmail.com>
2019-12-18 19:19:48 +1100

    .

--- src/components/sections/Contact/ContactForm/index.js
+++ src/components/sections/Contact/ContactForm/index.js
@@ -18,11 +18,9 @@ const ContactForm = ({
 		name="portfolio-dev"
 		method="post"
 		data-netlify="true"
-		data-netlify-recaptcha="true"
 		data-netlify-honeypot="bot-field"
 	>
-		<input type="hidden" name="bot-field" />
-		<div data-netlify-recaptcha="true"></div>
+		<input type="hidden" name="form-name" value="contact" />
 		<InputField>
 			<Input
 				as={FastField}
